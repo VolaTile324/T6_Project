ff2ffb2b40d581f84b257b2a0e2228cc6e9155b1
Faizeennn <muhammad1802ft.2020@student.uny.ac.id>
2023-06-06 16:14:51 +0700

    add quiz

---
+++ Assets/Scripts/Quiz/QuizData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class QuizData
+{
+    public string question;
+    public string[] answers;
+    public int correctAnswer;
+}
